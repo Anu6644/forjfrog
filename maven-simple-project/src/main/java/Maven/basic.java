d7b5f986163cc0ef8037183dccd0a0f0546c9dad
Anchoudhary <Anchoudhary@Ban-1Anchoudhary-Lt.corporate.local>
2018-08-29 11:43:52 +0530

    forjfrogcommit

---
+++ maven-simple-project/src/main/java/Maven/basic.java
@@ -0,0 +1,12 @@
+package Maven;
+
+public class basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "HI, this is my maven project";
+		System.out.println(str);
+
+	}
+
+}
