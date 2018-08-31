a2c1ab813ec870e590d3658da621fcc5708ede28
Anu6644 <34180960+Anu6644@users.noreply.github.com>
2018-08-31 10:51:32 +0530

    add

---
+++ maven-simple-project/src/main/java/Maven/add.java
@@ -0,0 +1,12 @@
+package Maven;
+
+public class add {
+	int c;
+	public int add(int a2, int b2) {
+		c = a2+b2;
+		System.out.println(c);
+		return c;
+	}
+	
+
+}
--- maven-simple-project/src/main/java/Maven/basic.java
+++ maven-simple-project/src/main/java/Maven/basic.java
@@ -1,12 +1,18 @@
 package Maven;
-
+import java.util.*;
 public class basic {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String str = "HI, this is my maven project";
 		System.out.println(str);
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter two numbers");
+		int a=sc.nextInt();
+		int b = sc.nextInt();
+		add obj = new add();
+		int c=obj.add(a,b);
+		System.out.println(c);
 	}
 
 }
